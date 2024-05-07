b762442974e72b37c0d25b3f4ef6ecbdbd76d507
Newton Neto <ncrneto@gmail.com>
2024-05-07 11:30:55 -0300

    Cria projeto da caoculadora no Xcode

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Newton Cardoso da Rocha Neto on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
