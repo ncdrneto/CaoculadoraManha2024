cb4d5a5a01c7aae9a636a037ad428a6884b4772d
Newton Neto <ncrneto@gmail.com>
2024-05-09 11:52:59 -0300

    Começa implementação do botão e adiciona imagem de Clarinha

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -10,17 +10,44 @@ import SwiftUI
 struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
+    @State var result: Int?
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão?")
+            
             Text("Anos")
             TextField("Quantos anos completos tem seu cão", value: $years, format: .number)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
+            
             Text("Meses")
             TextField("E quantos meses além disso ele tem", value: $months, format: .number)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
+            
+            Text("Porte")
+            
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+            
+            Button("Cãocular"){
+                print("cãocular")
+                result = 23
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .background(.indigo)
+            .foregroundStyle(.white)
+            .clipShape(.rect(cornerRadius: 10))
+            .bold()
         }
         .padding()
     }
