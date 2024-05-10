d4f1218dfdc52f273cb8f1b002363a0443ab107f
Newton Neto <ncrneto@gmail.com>
2024-05-10 11:53:36 -0300

    Implementa segmented control, lógica do botão e espaçamento melhor na tela

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
     @State var result: Int?
+    @State var porteSelecionado = "Pequeno"
+    let portes = ["Pequeno", "Médio", "Grande"]
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão?")
@@ -27,6 +29,13 @@ struct ContentView: View {
             
             Text("Porte")
             
+            Picker(selection: $porteSelecionado, label: Text("Portes")) {
+                ForEach(portes, id: \.self) { porte in 
+                    Text(porte)
+                }
+            }
+            .pickerStyle(.segmented)
+            
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
                 Text("\(result) anos")
@@ -38,10 +47,7 @@ struct ContentView: View {
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             }
             
-            Button("Cãocular"){
-                print("cãocular")
-                result = 23
-            }
+            Button("Cãocular", action: processYears)
             .frame(maxWidth: .infinity)
             .frame(height: 50)
             .background(.indigo)
@@ -50,7 +56,40 @@ struct ContentView: View {
             .bold()
         }
         .padding()
+        
     }
+    
+    func processYears() {
+        print("cãocular")
+        
+        guard let years, let months else {
+            print("Campos não preenchidos!")
+            return
+        }
+        
+        guard months > 0 || years > 0 else {
+            print("Pelo menos um campo deve ser maior que zero.")
+            return
+        }
+        
+        let multiplicador: Int
+        
+        switch porteSelecionado {
+        case "Pequeno":
+            multiplicador = 6
+        case "Médio":
+            multiplicador = 7
+        case "Grande":
+            multiplicador = 8
+        default:
+            multiplicador = 0
+        }
+        
+        result = years * multiplicador + months * multiplicador / 12
+  
+        
+    }
+
 }
 
 #Preview {
